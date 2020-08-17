42e8a251499af34eebb61ffd55252440124bcd68
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-17 04:41:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def euc_distance(x1,y1,x2,y2):
+  return (((x1-y1)**2)+((x2-y2)**2))**(1/2)
+
+print(euc_distance(4,3,4,3))
+
+# def test1(x1, y1):
+#   return (x1-y1)**2
+# print(test1(4,3))
+
+# def test1(x2, y2):
+#   return (x2-y2)**2
+# print(test1(4,3))
+
+print(2**(1/2))
